90d363841b088c9a7b3b0b5ab3a3724b26b36a6b
Sean-PC\Sean <yang.sean.66@gmail.com>
2018-02-22 15:03:13 -0800

    LC46 added sound

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -6,10 +6,12 @@ using UnityEngine;
 public class Rocket : MonoBehaviour {
 
     Rigidbody rigidBody;
+    AudioSource audioSource;
 
 	// Use this for initialization
 	void Start () {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -22,8 +24,9 @@ public class Rocket : MonoBehaviour {
         if (Input.GetKey(KeyCode.Space))
         {
             rigidBody.AddRelativeForce(Vector3.up);
-            print("up");
+            if (!audioSource.isPlaying) audioSource.Play();
         }
+        else audioSource.Stop();
         if (Input.GetKey(KeyCode.A))
         {
             transform.Rotate(Vector3.forward);
