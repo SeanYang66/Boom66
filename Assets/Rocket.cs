25c4b3720ee11803a01371df2f60540f793eed30
Sean-PC\Sean <yang.sean.66@gmail.com>
2018-02-27 16:52:45 -0800

    Course build finished

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,39 +1,141 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour {
+
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 100f;
+    [SerializeField] float levelLoadDelay = 2f;
+    [SerializeField] float deathDelay = 2f;
+
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip collisionSound;
+    [SerializeField] AudioClip nextLevel;
+
+    [SerializeField] ParticleSystem mainEngineParticles;
+    [SerializeField] ParticleSystem successParticles;
+    [SerializeField] ParticleSystem deathParticles;
+
 
     Rigidbody rigidBody;
     AudioSource audioSource;
 
-	// Use this for initialization
-	void Start () {
+    bool isTransitioning = false;
+    bool collisionOn = true;
+
+
+    // Use this for initialization
+    void Start () {
         rigidBody = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        ProcessInput();
-	}
-
-    private void ProcessInput()
-    {
-        if (Input.GetKey(KeyCode.Space))
+        if (!isTransitioning)
         {
-            rigidBody.AddRelativeForce(Vector3.up);
-            if (!audioSource.isPlaying) audioSource.Play();
+            RespondToRotateInput();
+            RespondToThrustInput();
         }
-        else audioSource.Stop();
+        if (Debug.isDebugBuild) RespondToDebugKeys();
+        
+    }
+
+    private void RespondToDebugKeys()
+    {
+        if (Input.GetKeyDown(KeyCode.L)) LoadNextScene();
+        if (Input.GetKeyDown(KeyCode.C)) collisionOn = !collisionOn;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (isTransitioning || !collisionOn) return;
+
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+
+                break;
+            case "Finish":
+                StartSuccessSequence();
+                break;
+            default:
+                StartDeathSequence();
+                break;
+        }
+    }
+
+    private void StartSuccessSequence()
+    {
+        isTransitioning = true;
+        Invoke("LoadNextScene", levelLoadDelay);
+        audioSource.Stop();
+        audioSource.PlayOneShot(nextLevel);
+        successParticles.Play();
+    }
+
+    private void StartDeathSequence()
+    {
+        isTransitioning = true;
+        Invoke("LoadFirstLevel", deathDelay);
+        audioSource.Stop();
+        audioSource.PlayOneShot(collisionSound);
+        deathParticles.Play();
+    }
+     
+    private void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings) nextSceneIndex = 0;
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void RespondToRotateInput()
+    {
+        rigidBody.angularVelocity = Vector3.zero;
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
         }
+        
+    }
+
+    private void RespondToThrustInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Thrust();
+        }
+        else
+        {
+            StopThrust();
+        }
+    }
+
+    private void StopThrust()
+    {
+        audioSource.Stop();
+        mainEngineParticles.Stop();
+    }
+
+    private void Thrust()
+    {
+        float thrustThisFrame = mainThrust * Time.deltaTime;
+        rigidBody.AddRelativeForce(Vector3.up * thrustThisFrame);
+        if (!audioSource.isPlaying) audioSource.PlayOneShot(mainEngine);
+        mainEngineParticles.Play();
     }
 }
